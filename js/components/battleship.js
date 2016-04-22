26c2b6a475722768aad5b4ade50994dbc18f170e
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-04-22 11:17:15 +0300

    remove table in favour of divs

--- js/components/battleship.js
+++ js/components/battleship.js
@@ -6,15 +6,20 @@ import CONFIG from '../../config/config.js';
 export default class Battleship extends React.Component {
   constructor(data) {
     super(data);
+    this.state = { deployed: false, selected: false }
   }
 
   onDragStart(event) {
-    console.log('dragg');
-    console.log(event.target);
+    event.dataTransfer.setData('ship', event.target.id);
+  }
+
+  onClick(e) {
+    console.log('clicked');
+    this.setState({ selected: true })
   }
 
   render() {
-    return <div className={this.props.type} draggable={true} onDragStart={this.onDragStart}>
+    return <div className={this.props.type} id={this.props.type} onClick={this.onClick.bind(this)}>
             {this.props.type}
            </div>
   }
--- js/components/board.js
+++ js/components/board.js
@@ -10,11 +10,26 @@ export default class Board extends React.Component {
     super(data);
   }
 
+  onDrop(event) {
+    console.log(event.target)
+    let ship = event.dataTransfer.getData('ship');
+    console.log(ship);
+    event.target.appendChild(document.getElementById(ship));
+  }
+
+  onMouseOver(e) {
+    console.log('mouseover');
+  }
+
+  allowDrop(event) {
+    event.preventDefault();
+  }
+
   render() {
     let tbody = range(ROWS).map((row) => {
       return <tr key={row} className="row-md-1">
         {range(COLUMNS).map((column) => {
-          return <td key={column} className="col-md-1">x</td>
+          return <td key={column} className="col-md-1" onDrop={this.onDrop} onDragOver={this.allowDrop}></td>
         })}
         </tr>
     });
@@ -22,13 +37,13 @@ export default class Board extends React.Component {
     let info = <b>{this.props.player} board</b>
 
     return (
-      <div>
+      <div className='board'>
       {info}
-      <table className="table table-bordered table-condensed">
-      <tbody>
-        {tbody}
-      </tbody>
-      </table>
+        <div className='board-grid'>
+          {range(ROWS).map((row) => {
+            return <div className='cell-grid'></div>
+          })}
+        </div>
       </div>
     )
   }

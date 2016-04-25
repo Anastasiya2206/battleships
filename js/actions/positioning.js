5128045218692f22e799622df9d8868ba15b9ed2
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-04-25 15:48:48 +0300

    add basic actions for redux

---
+++ js/actions/positioning.js
@@ -0,0 +1,10 @@
+const START_POSITIONING = 'START_POSITIONING';
+const END_POSITIONING = 'END_POSITIONING';
+
+const startPositioning = (battleship) => {
+  return { type: START_POSITIONING, battleship };
+}
+
+const endPositioning = (battleship) => {
+  return { type: END_POSITIONING, battleship };
+}
